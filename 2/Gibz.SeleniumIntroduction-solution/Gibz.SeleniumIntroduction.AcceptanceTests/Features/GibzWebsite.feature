697b498fe96f7b087306b5782051250cdb6c6cb4
asnafut <marcelhenggeler27@gmail.com>
2024-01-08 09:36:01 +0100

    add changes

---
+++ 2/Gibz.SeleniumIntroduction-solution/Gibz.SeleniumIntroduction.AcceptanceTests/Features/GibzWebsite.feature
@@ -0,0 +1,29 @@
+﻿Feature: GibzWebsite
+
+Scenario: Check Berufsverantwortliche from Informatiker/in EFZ
+	Given I open the GIBZ website
+	And I click on Grundbildung
+	And I click on Unsere Berufe
+	When I click on Informatiker/in EFZ
+	Then the first Berufsverantwortlicher is Peter Gisler
+	And the second Berufsverantwortlicher is Tobias Schmid
+
+Scenario: Check number of Berufe
+	Given I open the GIBZ website
+	And I click on Grundbildung
+	When I click on Unsere Berufe
+	Then <NumberOfBerufe> Berufe starting with <StartingKey> should be displayed
+Examples: 
+| NumberOfBerufe | StartingKey |
+| 5              | A           |
+| 1              | C           |
+| 4              | E           |
+| 3              | F           |
+| 1              | H           |
+| 2              | I           |
+| 3              | K           |
+| 2              | M           |
+| 1              | P           |
+| 1              | R           |
+| 2              | S           |
+| 3              | Z           |
---
+++ 2/Gibz.SeleniumIntroduction-solution/Gibz.SeleniumIntroduction.AcceptanceTests/StepDefinitions/GibzWebsiteSteps.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace Gibz.SeleniumIntroduction.AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public sealed class GibzWebsiteSteps
+    {
+        private ChromeDriver webDriver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            webDriver = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            webDriver.Quit();
+        }
+
+        [Given(@"I open the GIBZ website")]
+        public void GivenIOpenTheGibzWebsite()
+        {
+            webDriver.Navigate().GoToUrl("https://www.gibz.ch");
+        }
+
+        [Given(@"I click on Grundbildung")]
+        public void GivenIClickOnGrundbildung()
+        {
+            webDriver.FindElement(By.Id("grundbildung")).Click();
+        }
+
+        [Given(@"I click on Unsere Berufe")]
+        [When(@"I click on Unsere Berufe")]
+        public void GivenWhenIClickOnUnsereBerufe()
+        {
+            webDriver.FindElement(By.XPath("//li[@id='portaltab-grundbildung']/ul/li[2]/a")).Click();
+        }
+
+        [When(@"I click on Informatiker/in EFZ")]
+        public void WhenIClickOnInformatikerInEfz()
+        {
+            webDriver.FindElement(By.XPath("//a[@title='Informatiker/in EFZ']")).Click();
+        }
+
+        [Then(@"the first Berufsverantwortlicher is Peter Gisler")]
+        public void ThenTheFirstBerufsverantwortlicherIsPeterGisler()
+        {
+            var firstContactText = webDriver.FindElement(By.XPath("//a[@id='kontakt']//following-sibling::div[1]/p[1]")).Text;
+            var normalizedFirstContactText = NormalizeText(firstContactText);
+            normalizedFirstContactText.Should().Contain("Peter Gisler");
+        }
+
+        [Then(@"the second Berufsverantwortlicher is Tobias Schmid")]
+        public void ThenTheSecondBerufsverantwortlicherIsTobiasSchmid()
+        {
+            var secondContactText = webDriver.FindElement(By.XPath("//a[@id='kontakt']//following-sibling::div[1]/p[2]")).Text;
+            var normalizedSecondContactText = NormalizeText(secondContactText);
+            normalizedSecondContactText.Should().Contain("Tobias Schmid");
+        }
+
+        [Then(@"(.*) Berufe starting with (.*) should be displayed")]
+        public void ThenBerufeStartingWithShouldBeDisplayed(int expectedNumberOfBerufe, string startingKey)
+        {
+            var countLinks = webDriver.FindElements(By.XPath($"//div[@class = 'table-wrapper']//a[starts-with(text(), '{startingKey}')]")).Count;
+            var countSpans = webDriver.FindElements(By.XPath($"//div[@class = 'table-wrapper']//span[starts-with(text(), '{startingKey}')]")).Count;
+            var count = countLinks + countSpans;
+            count.Should().Be(expectedNumberOfBerufe);
+        }
+
+        private string NormalizeText(string input)
+        {
+            return input.Replace("­", "");
+        }
+    }
+}
